277d7147029c99432f78b70004ad9797c4b4f556
sadagi <sadagi.chandan@gmail.com>
2017-07-10 15:12:56 +0530

    Commiting all project changes

--- src/sadagi/utilities/examples/Commons.java
+++ src/sadagi/utilities/examples/Commons.java
@@ -9,9 +9,8 @@ public class Commons {
 	public static void main(String[] args) throws JSchException {
 	
 		
-		KSBUtility utility = new KSBUtility("root", "root123", "10.184.21.239", 22, "sftp");
+		KSBUtility utility = new KSBUtility("uersname", "password", "IP", 22, "sftp");
 		
-		//boolean b = utility.isExisTFile("/home/test/Yes.txt");
 		boolean b = utility.isExisTFile("/home/test/finall");
 		
 		System.out.println(b);
